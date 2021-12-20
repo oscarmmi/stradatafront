06443818523cc206321bd0af060f8a1b425462b6
Oscar Muñoz <oscarxxi@gmail.com>
2021-12-20 11:41:14 -0500

    Initial commit

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Stradata Front</title>
+
+        <link rel="stylesheet" href="librerias/css/bootstrap.min.css">
+        <script src="librerias/js/jquery-3.3.1.js"></script>
+        <script src="librerias/js/bootstrap.min.js"></script>
+        <script src="librerias/js/alertify.min.js"></script>
+        <!-- CSS -->
+        <link rel="stylesheet" href="librerias/css/alertify.min.css" />
+        <!-- Default theme -->
+        <link rel="stylesheet" href="librerias/css/default.min.css" />
+        <!-- Semantic UI theme -->
+        <link rel="stylesheet" href="librerias/css/semantic.min.css" />
+        <!-- Bootstrap theme -->
+        <link rel="stylesheet" href="librerias/css/alertify-bootstrap.min.css" />
+
+        <script src="librerias/js/Chart.bundle.min.js"></script>
+        <script src="librerias/js/Chart.js"></script>
+        <link rel="stylesheet" href="librerias/css/Chart.css"/>
+
+
+        <script type="text/javascript" src="librerias/js/jquery.dataTables.min.js"></script>
+        <script type="text/javascript" src="librerias/js/dataTables.select.min.js"></script>
+        <script type="text/javascript" src="librerias/js/dataTables.buttons.min.js"></script>
+
+        <link rel="stylesheet" href="librerias/css/jquery.dataTables.min.css" />
+        <link rel="stylesheet" href="librerias/css/select.dataTables.min.css" />
+        <link rel="stylesheet" href="librerias/css/buttons.dataTables.min.css" />
+
+        <script src="librerias/js/funciones.js"></script>
+    </head>
+    <body>
+        <div class="container">
+            <div class="row top-buffer">
+
+            </div>    
+        </div>
+    </body>
+</html>
